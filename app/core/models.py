7f5a185adbec9e4176d74377928189090d088d6a
Jose Ruiz <TuunicoApp@gmail.com>
2020-07-30 12:51:01 -0400

    Added working list tags endpoint

--- app/core/models.py
+++ app/core/models.py
@@ -2,6 +2,7 @@ from django.db import models
 from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, \
                                         PermissionsMixin
 
+from django.conf import settings
 
 # Create your models here.
 class UserManager(BaseUserManager):
@@ -36,3 +37,46 @@ class User(AbstractBaseUser, PermissionsMixin):
     objects = UserManager()
 
     USERNAME_FIELD = 'email'
+
+
+class Tag(models.Model):
+    """Tag to be used for a recipe"""
+    name = models.CharField(max_length=255)
+    user = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+    )
+      # In settings AUTH_USER_MODEL = 'core.User'
+
+    def __str__(self):
+        return self.name
+
+
+# class Ingredient(models.Model):
+#     """Ingredient to be used in a recipe"""
+#     name = models.CharField(max_length=255)
+#     user = models.ForeignKey(
+#         settings.AUTH_USER_MODEL,
+#         on_delete=models.CASCADE
+#     )
+
+#     def __str__(self):
+#         return self.name
+
+
+# class Recipe(models.Model):
+#     """Recipe object"""
+#     user = models.ForeignKey(
+#         settings.AUTH_USER_MODEL,
+#         on_delete=models.CASCADE
+#     )
+#     title = models.CharField(max_length=255)
+#     time_minutes = models.IntegerField()
+#     price = models.DecimalField(max_digits=5, decimal_places=2)
+#     link = models.CharField(max_length=255, blank=True)
+#     ingredients = models.ManyToManyField('Ingredient')
+#     tags = models.ManyToManyField('Tag')
+#     image = models.ImageField(null=True, upload_to=recipe_image_file_path)
+
+#     def __str__(self):
+#         return self.title
--- app/core/tests/test_model.py
+++ app/core/tests/test_model.py
@@ -1,7 +1,7 @@
 from django.test import TestCase
 from django.contrib.auth import get_user_model
 
-# from core import models
+from core import models
 
 
 def sample_user(email='test@londonappdev.com', password='testpass'):
@@ -44,3 +44,42 @@ class ModelTests(TestCase):
 
         self.assertTrue(user.is_superuser)
         self.assertTrue(user.is_staff)
+
+    def test_tag_str(self):
+        """Test the tag string representation"""
+        tag = models.Tag.objects.create(
+            user=sample_user(),
+            name='Vegan'
+        )
+
+        self.assertEqual(str(tag), tag.name)
+
+    # def test_ingredient_str(self):
+    #     """Test the ingredient string respresentation"""
+    #     ingredient = models.Ingredient.objects.create(
+    #         user=sample_user(),
+    #         name='Cucumber'
+    #     )
+
+    #     self.assertEqual(str(ingredient), ingredient.name)
+
+    # def test_recipe_str(self):
+    #     """Test the recipe string representation"""
+    #     recipe = models.Recipe.objects.create(
+    #         user=sample_user(),
+    #         title='Steak and mushroom sauce',
+    #         time_minutes=5,
+    #         price=5.00
+    #     )
+
+    #     self.assertEqual(str(recipe), recipe.title)
+
+    # @patch('uuid.uuid4')
+    # def test_recipe_file_name_uuid(self, mock_uuid):
+    #     """Test that image is saved in the correct location"""
+    #     uuid = 'test-uuid'
+    #     mock_uuid.return_value = uuid
+    #     file_path = models.recipe_image_file_path(None, 'myimage.jpg')
+
+    #     exp_path = f'uploads/recipe/{uuid}.jpg'
+    #     self.assertEqual(file_path, exp_path)
