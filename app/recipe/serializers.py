21de1bc2b01248fd43a2b371afcc4169bf4ae19b
Jose Ruiz <TuunicoApp@gmail.com>
2020-08-04 13:02:12 -0400

    Added recipe image upload feature

--- app/recipe/serializers.py
+++ app/recipe/serializers.py
@@ -49,10 +49,10 @@ class RecipeDetailSerializer(RecipeSerializer):
     tags = TagSerializer(many=True, read_only=True)
 
 
-# class RecipeImageSerializer(serializers.ModelSerializer):
-#     """Serializer for uploading images to recipes"""
+class RecipeImageSerializer(serializers.ModelSerializer):
+    """Serializer for uploading images to recipes"""
 
-#     class Meta:
-#         model = Recipe
-#         fields = ('id', 'image')
-#         read_only_fields = ('id',)
+    class Meta:
+        model = Recipe
+        fields = ('id', 'image')
+        read_only_fields = ('id',)
--- app/recipe/views.py
+++ app/recipe/views.py
@@ -1,6 +1,6 @@
-# from rest_framework.decorators import action
-# from rest_framework.response import Response
-from rest_framework import viewsets, mixins
+from rest_framework.decorators import action
+from rest_framework.response import Response
+from rest_framework import viewsets, mixins, status
 from rest_framework.authentication import TokenAuthentication
 from rest_framework.permissions import IsAuthenticated
 
@@ -63,6 +63,8 @@ class RecipeViewSet(viewsets.ModelViewSet):
         """Return appropriate serializer class"""
         if self.action == 'retrieve':
             return serializers.RecipeDetailSerializer
+        elif self.action == 'upload_image':
+            return serializers.RecipeImageSerializer
 
         return self.serializer_class
 
@@ -70,6 +72,26 @@ class RecipeViewSet(viewsets.ModelViewSet):
         """Create a new recipe"""
         serializer.save(user=self.request.user)
 
+    @action(methods=['POST'], detail=True, url_path='upload-image')
+    def upload_image(self, request, pk=None):
+        """Upload an image to a recipe"""
+        recipe = self.get_object()
+        serializer = self.get_serializer(
+            recipe,
+            data=request.data
+        )
+
+        if serializer.is_valid():
+            serializer.save()
+            return Response(
+                serializer.data,
+                status=status.HTTP_200_OK
+            )
+
+        return Response(
+            serializer.errors,
+            status=status.HTTP_400_BAD_REQUEST
+        )
 
 #     def _params_to_ints(self, qs):
 #         """Convert a list of string IDs to a list of integers"""
@@ -88,36 +110,6 @@ class RecipeViewSet(viewsets.ModelViewSet):
 #             queryset = queryset.filter(ingredients__id__in=ingredient_ids)
 
 #         return queryset.filter(user=self.request.user)
-
-    # def get_serializer_class(self):
-    #     """Return appropriate serializer class"""
-    #     if self.action == 'retrieve':
-    #         return serializers.RecipeDetailSerializer
-    #     elif self.action == 'upload_image':
-    #         return serializers.RecipeImageSerializer
-
-    #     return self.serializer_class
-
-#     @action(methods=['POST'], detail=True, url_path='upload-image')
-#     def upload_image(self, request, pk=None):
-#         """Upload an image to a recipe"""
-#         recipe = self.get_object()
-#         serializer = self.get_serializer(
-#             recipe,
-#             data=request.data
-#         )
-
-#         if serializer.is_valid():
-#             serializer.save()
-#             return Response(
-#                 serializer.data,
-#                 status=status.HTTP_200_OK
-#             )
-
-#         return Response(
-#             serializer.errors,
-#             status=status.HTTP_400_BAD_REQUEST
-#         )
 
 # OLD VERSIONS --------------------------------------------------
 
